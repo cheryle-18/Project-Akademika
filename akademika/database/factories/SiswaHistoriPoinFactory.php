40d0526d0f67fec4f70f81658bc8230a6b684d42
Angelizines <angelita_j20@mhs.istts.ac.id>
2022-11-19 17:19:35 +0700

    init factory

---
+++ akademika/database/factories/SiswaHistoriPoinFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SiswaHistoriPoin>
+ */
+class SiswaHistoriPoinFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
