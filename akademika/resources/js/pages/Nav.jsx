097dc59d66b1ea57d21e7d708d85d7104f7fe605
Jonathan Theja <105841081+JonathanTheja@users.noreply.github.com>
2022-11-23 23:05:06 +0700

    alert masuk daftar

--- akademika/resources/js/pages/Nav.jsx
+++ akademika/resources/js/pages/Nav.jsx
@@ -4,11 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import AuthUser from "../components/AuthUser";
+import { Alert } from "@material-tailwind/react";
 
 const Nav = () => {
+    const [isLoginFailed, setLoginFailed] = useState(false);
+    const [registerFailed, setRegisterFailed] = useState("success");
     const [isDaftar, setDaftar] = useState(true);
     //login attribs
-    const {http,setToken} = AuthUser();
+    const { http, setToken } = AuthUser();
     const [loginEmail, setLoginEmail] = useState();
     const [loginPassword, setLoginPassword] = useState();
 
@@ -24,60 +27,84 @@ const Nav = () => {
     const changeDaftarTrue = () => {
         setDaftar(true);
         document.body.style.overflow = "hidden";
+        setLoginFailed(false);
+        setRegisterFailed("success");
     };
 
     const changeDaftarFalse = () => {
         setDaftar(false);
         document.body.style.overflow = "hidden";
+        setLoginFailed(false);
+        setRegisterFailed("success");
     };
 
     const changeScroll = () => {
         document.body.style.overflow = "auto";
-    }
+    };
     const submitLoginForm = () => {
-
         //api call
-        http.post('/login', { email: loginEmail, password: loginPassword }).then((res) => {
-          let data = res.data;
+        http.post("/login", {
+            email: loginEmail,
+            password: loginPassword,
+        }).then((res) => {
+            let data = res.data;
 
-          if (data.access_token != null && data.user != null) {
-            //login success
-            setToken(res.data.user, res.data.access_token);
-            console.log(data.user);
-          }
-          else {
-            alert(data);
-          }
-        })
-    }
+            if (data.access_token != null && data.user != null) {
+                //login success
+                setToken(res.data.user, res.data.access_token);
+                document.body.style.overflow = "auto";
+                console.log(data.user);
+            } else {
+                setLoginFailed(true);
+                alert(data);
+            }
+        });
+    };
 
     const submitRegisterForm = () => {
         //api call
-        http.post('/register',{nama:registerNama,username:registerUsername,telp:registerTelp, email:registerEmail,password:registerPassword,confirm_password:registerConfirmPassword,role:registerRole}).then((res) => {
-          let data = res.data;
+        http.post("/register", {
+            nama: registerNama,
+            username: registerUsername,
+            telp: registerTelp,
+            email: registerEmail,
+            password: registerPassword,
+            confirm_password: registerConfirmPassword,
+            role: registerRole,
+        }).then((res) => {
+            let data = res.data;
+            setRegisterFailed(data);
             console.log(data);
-        })
-    }
+            if(data=="success"){
+                setDaftar(false);
+            }
+        });
+    };
 
     const cetakDaftar = (
         <div>
+            {registerFailed!="success" && (
+                <Alert severity="error" className="bg-red-400 mb-6">
+                    {registerFailed}
+                </Alert>
+            )}
             <h3 className="text-3xl font-bold text-custom-blue">
                 Daftar Akademika
             </h3>
-            <p className="py-4">
+            <p className="py-2">
                 <input
                     type="text"
                     placeholder="Nama Lengkap"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterNama(e.target.value)}
+                    onChange={(e) => setRegisterNama(e.target.value)}
                 />
             </p>
-            <p className="py-4">
+            <p className="py-2">
                 <input
                     type="text"
                     placeholder="Username"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterUsername(e.target.value)}
+                    onChange={(e) => setRegisterUsername(e.target.value)}
                 />
             </p>
             <p className="py-2">
@@ -85,7 +112,7 @@ const Nav = () => {
                     type="text"
                     placeholder="Email"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterEmail(e.target.value)}
+                    onChange={(e) => setRegisterEmail(e.target.value)}
                 />
             </p>
             <p className="py-2">
@@ -93,7 +120,7 @@ const Nav = () => {
                     type="text"
                     placeholder="Telp"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterTelp(e.target.value)}
+                    onChange={(e) => setRegisterTelp(e.target.value)}
                 />
             </p>
             <p className="py-2">
@@ -101,7 +128,7 @@ const Nav = () => {
                     type="text"
                     placeholder="Password"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterPassword(e.target.value)}
+                    onChange={(e) => setRegisterPassword(e.target.value)}
                 />
             </p>
             <p className="py-2">
@@ -109,7 +136,7 @@ const Nav = () => {
                     type="text"
                     placeholder="Konfirmasi Password"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setRegisterConfirmPassword(e.target.value)}
+                    onChange={(e) => setRegisterConfirmPassword(e.target.value)}
                 />
             </p>
             <div className="pt-2">
@@ -119,7 +146,7 @@ const Nav = () => {
                     id="guru"
                     name="fav_language"
                     value="guru"
-                    onChange={e => setRegisterRole(e.target.value)}
+                    onChange={(e) => setRegisterRole(e.target.value)}
                 />
                 <label htmlFor="guru">&nbsp;Guru</label>
                 &nbsp;&nbsp;&nbsp;&nbsp;
@@ -128,11 +155,14 @@ const Nav = () => {
                     id="siswa"
                     name="fav_language"
                     value="siswa"
-                    onChange={e => setRegisterRole(e.target.value)}
+                    onChange={(e) => setRegisterRole(e.target.value)}
                 />
                 <label htmlFor="siswa">&nbsp;Siswa</label>
             </div>
-            <div className="w-full py-2 mt-4 bg-custom-blue text-white text-center rounded-md" onClick={submitRegisterForm}>
+            <div
+                className="w-full py-2 mt-4 bg-custom-blue text-white text-center rounded-md cursor-pointer"
+                onClick={submitRegisterForm}
+            >
                 Buat Akun
             </div>
             <div className="w-full mt-4 flex justify-center text-custom-blue">
@@ -143,19 +173,23 @@ const Nav = () => {
                 >
                     Masuk Sekarang
                 </div>
-
             </div>
         </div>
     );
     const cetakMasuk = (
         <div>
+            {isLoginFailed && (
+                <Alert severity="error" className="bg-red-400 mb-6">
+                    Login Failed
+                </Alert>
+            )}
             <h3 className="text-3xl font-bold text-custom-blue">Masuk</h3>
             <p className="py-4">
                 <input
                     type="text"
                     placeholder="Email"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setLoginEmail(e.target.value)}
+                    onChange={(e) => setLoginEmail(e.target.value)}
                     required
                 />
             </p>
@@ -164,14 +198,15 @@ const Nav = () => {
                     type="text"
                     placeholder="Password"
                     className="input input-bordered w-full border-2 border-gray-500 rounded-md placeholder-gray-700"
-                    onChange={e => setLoginPassword(e.target.value)}
+                    onChange={(e) => setLoginPassword(e.target.value)}
                     required
                 />
             </p>
             <div className="w-full">
                 <div className="text-right text-custom-blue">Lupa Password</div>
             </div>
-            <div className="w-full py-2 mt-4 bg-custom-blue text-white text-center rounded-md"
+            <div
+                className="w-full py-2 mt-4 bg-custom-blue text-white text-center rounded-md cursor-pointer"
                 onClick={submitLoginForm}
             >
                 Masuk
@@ -210,8 +245,7 @@ const Nav = () => {
                         Masuk
                     </label>
                     <div className="pt-1.5 text-white float-right mr-5 cursor-pointer">
-                    <Link to="/kursus/search">Cari Kursus</Link>
-
+                        <Link to="/kursus/search">Cari Kursus</Link>
                     </div>
                 </div>
                 <div className="md:hidden w-full">
@@ -223,12 +257,20 @@ const Nav = () => {
                             Cari Kursus
                         </div>
                     </div>
-                    <label htmlFor="masukDaftar" onClick={changeDaftarFalse} className="flex justify-center align-center">
+                    <label
+                        htmlFor="masukDaftar"
+                        onClick={changeDaftarFalse}
+                        className="flex justify-center align-center"
+                    >
                         <div className="cursor-pointer border-2 border-white bg-transparent text-white py-1 px-8 rounded-md hover:bg-custom-blue hover:text-white bg-white w-full sm:w-96 text-center mt-2">
                             Masuk
                         </div>
                     </label>
-                    <label htmlFor="masukDaftar" onClick={changeDaftarTrue} className="flex justify-center align-center">
+                    <label
+                        htmlFor="masukDaftar"
+                        onClick={changeDaftarTrue}
+                        className="flex justify-center align-center"
+                    >
                         <div className="cursor-pointer border-2 border-white text-custom-blue py-1 px-8 rounded-md hover:bg-custom-blue hover:text-white bg-white w-full sm:w-96 text-center mt-2">
                             Daftar
                         </div>
@@ -237,12 +279,17 @@ const Nav = () => {
             </div>
 
             <div className="z-10">
-                <input type="checkbox" id="masukDaftar" className="modal-toggle" />
+                <input
+                    type="checkbox"
+                    id="masukDaftar"
+                    className="modal-toggle"
+                />
                 <div className="modal">
                     <div className="modal-box relative py-10 px-8">
                         <label
                             htmlFor="masukDaftar"
-                            className="btn btn-sm absolute bg-transparent text-gray-500 border border-none hover:bg-transparent hover:border-none right-2 top-2 font-bold text-xl" onClick={changeScroll}
+                            className="btn btn-sm absolute bg-transparent text-gray-500 border border-none hover:bg-transparent hover:border-none right-2 top-2 font-bold text-xl"
+                            onClick={changeScroll}
                         >
                             ✕
                         </label>
