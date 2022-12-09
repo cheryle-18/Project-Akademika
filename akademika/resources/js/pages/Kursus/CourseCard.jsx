a559196069f6cccf26c9a0270d3fc1ca5876ad40
Jonathan Theja <105841081+JonathanTheja@users.noreply.github.com>
2022-12-09 07:59:54 +0700

    fix kursus search guest

--- akademika/resources/js/pages/Kursus/CourseCard.jsx
+++ akademika/resources/js/pages/Kursus/CourseCard.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import clockLogo from "../../../images/Clock.png";
 import CardPic from "../../../images/card_pic.png";
 import { round } from "lodash";
-import {toRupiah} from "../../components/CurrencyUtils";
+import { toRupiah } from "../../components/CurrencyUtils";
 import { Link } from "react-router-dom";
 import AuthUser from "../../components/AuthUser";
 
 const CourseCard = (props) => {
-    console.log(props)
-    const {http,user} = AuthUser()
-    return(
+    console.log(props);
+    const { http, user } = AuthUser();
+    return (
         <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg w-1/2 h-auto">
             <img
                 className=" w-full h-full md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
@@ -38,10 +38,26 @@ const CourseCard = (props) => {
                         {round(props.course.durasi / 60)} jam
                     </span>
                     <span>
-                        <Link to={
-                            ((props.status == "diterbitkan" && user.role_text == "guru") && "/guru/kursus/"+props.course.kursus_id+"/home") ||
-                            ((props.status != "diterbitkan" && user.role_text == "guru") && "/guru/kursus/"+props.course.kursus_id+"/detail") || ((user.role_text == "siswa") && "/siswa/kursus/"+props.course.kursus_id+"/detail")
-                            } className="ml-auto">
+                        <Link
+                            to={
+                                user != null &&
+                                ((props.status == "diterbitkan" &&
+                                    user.role_text == "guru" &&
+                                    "/guru/kursus/" +
+                                        props.course.kursus_id +
+                                        "/home") ||
+                                    (props.status != "diterbitkan" &&
+                                        user.role_text == "guru" &&
+                                        "/guru/kursus/" +
+                                            props.course.kursus_id +
+                                            "/detail") ||
+                                    (user.role_text == "siswa" &&
+                                        "/siswa/kursus/" +
+                                            props.course.kursus_id +
+                                            "/detail"))
+                            }
+                            className="ml-auto"
+                        >
                             <button
                                 type="button"
                                 class="inline-block ml-auto px-6 py-2.5 bg-custom-blue text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-800 hover:shadow-lg focus:bg-blue-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
@@ -53,7 +69,7 @@ const CourseCard = (props) => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default CourseCard;
